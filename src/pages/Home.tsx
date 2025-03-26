5695066b4a1c0f272592e393f7aa42e15b61a2e8
oyewosolomon <oyewosolomon@gmail.com>
2025-03-26 15:40:11 +0100

    Updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,9 @@
 import Hero from "@/components/sections/Hero";
-import Features from "@/components/sections/Features";
-import Analytics from "@/components/sections/Analytics";
-import Success from "@/components/sections/Success";
-import Team from "@/components/sections/Team";
 import Contact from "@/components/sections/Contact";
-import Pricing from "@/components/sections/Pricing";
-import Partners from "@/components/sections/Partners";
-import FAQ from "@/components/sections/FAQ";
+
 import ProductDemo from "@/components/sections/ProductDemo";
 import Footer from "@/components/layout/Footer";
 import AboutUs from "@/components/sections/AboutUs";
-import Testimonials from "@/components/sections/Testimonials";
 import Products from "@/components/sections/Products";
 import Industries from "@/components/sections/Industries";
 import Services from "@/components/sections/Services";
@@ -24,9 +17,6 @@ export default function Home() {
       <Services/>
       <Products/>
       <ProductDemo />
-      <Success/>
-      {/* <Testimonials/> */}
-      <Team />
       <Contact />
       <Footer/>
     </main>
